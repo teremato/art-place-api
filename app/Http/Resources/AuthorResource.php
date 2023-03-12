951439c1ec5cf682358808decb305de11b21778b
teremato <everosha.animation@gmail.com>
2023-03-12 16:52:54 +0500

    Перезагрузка бека

--- app/Http/Resources/AuthorResource.php
+++ app/Http/Resources/AuthorResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserResourse extends JsonResource
+class AuthorResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -12,13 +12,8 @@ class UserResourse extends JsonResource
      * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
-
-    public function toArray($request) {
-        
-        return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'email' => $this->email
-        ];
+    public function toArray($request)
+    {
+        return parent::toArray($request);
     }
 }
